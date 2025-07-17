fa2e55d9fa0201359e0467074bf1c021bfb6ca5d
Aman911kumar@ <ak9876794207@gmail.com>
2025-07-17 18:54:11 +0530

    add comment, like, playlist, and tweet models with schemas for content management

---
+++ src/models/palylist.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const playlistSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        retquired: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    videos: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, { timestamps: true })
+
+export const Playlist = new mongoose.model("Playlist", playlistSchema) 
